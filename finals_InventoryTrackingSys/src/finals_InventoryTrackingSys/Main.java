c03144979e1ad28f168f83b9ae9dc3a4ed20fb4f
Jerry Aaron <Jerry Aaron@Alphie>
2026-04-28 13:45:48 +0800

    Update: UI and new Bold Texts Feats. !!

--- finals_InventoryTrackingSys/src/finals_InventoryTrackingSys/Main.java
+++ finals_InventoryTrackingSys/src/finals_InventoryTrackingSys/Main.java
@@ -8,13 +8,15 @@ public class Main {
     	
         String c_Red = "\u001B[31m";
         String c_Green = "\u001B[32m";
+        String b_Texts = "\u001B[1m";
+        String u_Texts = "\u001B[4m";
         String c_Reset = "\u001B[0m";
-
+        
         try{
         	// Instance Variables
         	boolean ContinueOpt = true;
         	int loopChoice;
-        	int countFormat = 115;
+        	
         	
         	// Method
         	PrintWriter print = new PrintWriter(System.out);
@@ -22,38 +24,51 @@ public class Main {
         	//Logic_Inventory METHOD
         	Logics_Inventory opts = new Logics_Inventory();
         	
-        	
+        	// UI Format: 
+        	int countFormat = 53;
         	print.println("\n");
-        	print.println("\t " + "—".repeat(300));
+        	print.println("\t " + b_Texts + "—".repeat(172) + c_Reset);
+        	final String repetition57 = " ".repeat(54);
+        	final String repetition59 = " ".repeat(56);
+        	final String centerRepeat = " ".repeat(75);
+        	final String subCenterRepeat = " ".repeat(55);
+
+        	final String tab = "\t";
+        	final String tabSpace = "\t ";
+        	final String tabLine = "\t | ";
         	String welcome = 
         			"\t | " + " ".repeat(countFormat) + """
-        			██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗""" + " ".repeat(120) + "| \n"  
+        			██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗""" + repetition57 + "| \n"  
         			
         	        + "\t | " + " ".repeat(countFormat) + """
-        			██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝""" + " ".repeat(120) + "| \n" 
+        			██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝""" + repetition57 + "| \n" 
         	        
         			+ "\t | " + " ".repeat(countFormat) + """
-        	        ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗""" + " ".repeat(122) + "| \n" 
+        	        ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗""" + repetition59 + "| \n" 
         			
         	        + "\t | " +  " ".repeat(countFormat) + """
-        	        ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝""" + " ".repeat(122) + "| \n" 
+        	        ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝""" + repetition59 + "| \n" 
         	        
         	        + "\t | " +  " ".repeat(countFormat) + """
-        	        ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗""" + " ".repeat(120) + "| \n" 
+        	        ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗""" + repetition57 + "| \n" 
         	        
         	        + "\t | " +  " ".repeat(countFormat + 1) + """
-        	         ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝""" + " ".repeat(121) + "|" +
+        	         ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝""" + repetition57 + "|" +
         	        """
         	        """;
         	
-        	String inventoryHeader;
+        		/*
+        		 String inventoryHeader =
+        		 "\t | + """ 
+        		 """;
+        		 */
+        	
         	
         	print.println(welcome);
-        	print.println("\t" + "—".repeat(300));
-        	print.println("\t"+ "█".repeat(300));
+        	print.println("\t " + "—".repeat(172));
+        	print.println("\t " + "█".repeat(172));
         	
         	print.flush();
-        	System.out.println("\t | ");
         	
         	while(ContinueOpt){
         		 
@@ -61,12 +76,25 @@ public class Main {
 	        	Scanner scan = new Scanner (System.in);
 	        		        	
 	            // WHAT TO ACCESS, ASKED TO THE USER
-	            System.out.println("\t | Enter What to Access: ");
-	            System.out.println("\t | [1] Inventory ");
-	            System.out.println("\t | [2] Item ");
-	            System.out.println("\t | [3] Group or Category ");
-	            System.out.println("\t | [4] Exit the Program ");
-	            System.out.print("\t | —> ");
+	        	System.out.println("\n");
+	        	System.out.println(tabSpace + " " + "_".repeat(169));
+	        	System.out.println(tabSpace);
+	        	System.out.println(centerRepeat +  b_Texts + "========================================" + c_Reset);
+	            System.out.println(tabSpace + centerRepeat + "  " + b_Texts + "CHOOSE TO ACCESS" + "   " + c_Reset);
+	            
+	            String firstLineOption = (tabSpace + subCenterRepeat + "[" + b_Texts + "1" + c_Reset +  "] " + b_Texts + "Inventory " + c_Reset);
+	            String secondtLineOption = (tabSpace + " ".repeat(25) + "[" + b_Texts + "2" + c_Reset +  "] " + b_Texts + "Item " + c_Reset);
+	            String thirdtLineOption = (tabSpace + subCenterRepeat + "[" + b_Texts + "3" + c_Reset +  "] " + b_Texts + "Group or Category " + c_Reset);
+	            String fourthtLineOption = (tabSpace + " ".repeat(17) + "[" + b_Texts + "4" + c_Reset +  "] " + b_Texts + "Exit the Program " + c_Reset);
+	            String fifthLineScan = (tabSpace + subCenterRepeat + b_Texts +  "—> " + c_Reset);
+	            
+	            print.print(firstLineOption);
+	            print.println(secondtLineOption);
+	            print.print(thirdtLineOption);	            
+	            print.println(fourthtLineOption);
+	            print.print(fifthLineScan);
+	            print.flush();
+	            
 	            String switch_Control = scan.nextLine();
 	            opts.setFirstDecisions(switch_Control);
 	            
@@ -76,7 +104,6 @@ public class Main {
                     	Logics_Inventory.chooseInventoryOption();
                     	// FUNCTION TO BE CALLED DEPENDING ON THE PREVIOUS DECISION
                     	Logics_Inventory.getSecondDecisions();
-            	        
                     	break;
                     	
                     case ITEM:
