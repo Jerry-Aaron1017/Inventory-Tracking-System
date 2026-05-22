b7fa84b094dd974a02116f4a95eab190898f24d3
Jerry Aaron <jerryaaronsaquibal@gmail.com>
2026-05-22 23:19:30 +0800

    Main may22.2 (#14)
    * UPDATE: DASHBOARD IN INVENTORY MODULE; UI and Variables for Spacing
    
    * UPDATE: OUTLINE OF THE ITEMS in the INVENTORY and its UI
    
    * UPDATE: ADJUSTEMENTS TO THE INVENTORY OUTLINE LIST and its UI;
    
    * UPDATE; UI and its Variables; Adjustment of the UI on Adding an Item
    Module; Implemented Similar Control Flow(Conditional Loop) Try Again for
    the user on other Methods; etc.
    
    * UPDATE: Progress is current to the ITEM DECISION-TO-ACCESS Module, new
    comments to suggest control flows or consider circumstances of
    user-to-system process, new methods for ITEM DTA Module, and need to
    creat fourth InsVar for GROUP DTA Module, etc.
    
    p.s. Consider using inheritance for next features.
    
    * UPDATE: Enchanced UI(Not Final), current work and progress is in the
    Item Module
    
    * UPDATE: Almost Same With May 16
    
    * UPDATE: PROPER Flow for Password Verification
    
    * UPDATE: Managing the Accounts, and Creating another METHODS and
    PREDEFINED PROCESSES -> NEXT:SEO for Accs. for Retrieval and Editing or
    deleting
    
    * UPDATE: Majorly in Managing accounts, Next is Creating new Account
    method and having a SEO for the accounts that might duplicate and for
    editing the accounts.
    
    * Major Cleaning of Flow: Current Progress is in Items and Groups
    
    * UPDATED FLOW
    
    * UPDATE ALMOST AT 75% haha
    
    ---------
    
    Co-authored-by: Jerry Aaron <Jerry Aaron@Alphie>

--- finals_InventoryTrackingSys/src/finals_InventoryTrackingSys/Main.java
+++ finals_InventoryTrackingSys/src/finals_InventoryTrackingSys/Main.java
@@ -10,51 +10,53 @@ public class Main {
     	
         try{
         	Logics_Inventory.profileUser();
-        	Logics_Inventory.firstDecision();
-        	
-        	while(ContinueOpt){
-                switch(Logics_Inventory.returnCons()){
-                    case INVENTORY:
-                    	// SECOND DECISION
-                    	Logics_Inventory.chooseInventoryOption();
-                    	// FUNCTION TO BE CALLED DEPENDING ON THE PREVIOUS DECISION
-                    	Logics_Inventory.getSecondDecisions();
-                    	break;
-                    	
-                    case ITEM:
-                    	// SECOND DECISION
-                    	Logics_Inventory.chooseItemOption();
-                    	// FUNCTION TO BE CALLED DEPENDING ON THE PREVIOUS DECISION
-                    	Logics_Inventory.getThirdDecision();
-                    	
-                    	break;
-                    	
-                    case GROUP, CATEGORY:
-                    	System.out.println("\t | ACCESS A GROUP OR CATEGORY");
-                		break;
-            		
-                    case EXIT:
-                    	Logics_Inventory.profileUser();
-                    	Logics_Inventory.firstDecision();
-                    	break;
-            			
-                    case DEFAULT:
-                    	System.out.println("\t | EXITING SYSTEM");
-            			break;
-            			
-                    default:
-            			System.out.println("\t | INVALID INPUT");
-            			break;
-                }
-                
-                if (Logics_Inventory.returnCons().toString().equalsIgnoreCase("exit")) {
-                	break;
-                }
-                //else if (Logics_Inventory.returnCons().toString().equalsIgnoreCase(""))
-                else {
-                	break;
-                }
-            }      
+        	if(Logics_Inventory.continueAccessSystem) {
+        		Logics_Inventory.firstDecision();
+
+	        	while(ContinueOpt){
+	                switch(Logics_Inventory.returnCons()){
+	                    case INVENTORY:
+	                    	// SECOND DECISION
+	                    	Logics_Inventory.chooseInventoryOption();
+	                    	// FUNCTION TO BE CALLED DEPENDING ON THE PREVIOUS DECISION
+	                    	Logics_Inventory.getSecondDecisions();
+	                    	break;
+	                    	
+	                    case ITEM:
+	                    	// SECOND DECISION
+	                    	Logics_Inventory.chooseItemOption();
+	                    	// FUNCTION TO BE CALLED DEPENDING ON THE PREVIOUS DECISION
+	                    	Logics_Inventory.getThirdDecision();
+	                    	
+	                    	break;
+	                    	
+	                    case GROUP, CATEGORY:
+	                    	System.out.println("\t | ACCESS A GROUP OR CATEGORY");
+	                		break;
+	            		
+	                    case EXIT:
+	                    	Logics_Inventory.profileUser();
+	                    	Logics_Inventory.firstDecision();
+	                    	break;
+	            			
+	                    case DEFAULT:
+	                    	System.out.println("\t | EXITING SYSTEM");
+	            			break;
+	            			
+	                    default:
+	            			System.out.println("\t | INVALID INPUT");
+	            			break;
+	                }
+	                
+	                if (Logics_Inventory.returnCons().toString().equalsIgnoreCase("exit")) {
+	                	break;
+	                }
+	                //else if (Logics_Inventory.returnCons().toString().equalsIgnoreCase(""))
+	                else {
+	                	break;
+	                }
+	            }  
+        	}
     	}
         
         catch(Exception e){
