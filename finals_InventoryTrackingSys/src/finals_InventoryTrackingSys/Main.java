f120916abb0943e23bec7fae4d36f043d34e5e4a
Jerry Aaron <jerryaaronsaquibal@gmail.com>
2026-05-24 14:31:11 +0800

    Main may24! (#16)
    * UPDATE: DASHBOARD IN INVENTORY MODULE; UI and Variables for Spacing
    
    * UPDATE: OUTLINE OF THE ITEMS in the INVENTORY and its UI
    
    * UPDATE: ADJUSTEMENTS TO THE INVENTORY OUTLINE LIST and its UI;
    
    * UPDATE; UI and its Variables; Adjustment of the UI on Adding an Item
    Module; Implemented Similar Control Flow(Conditional Loop) Try Again for
    the user on other Methods; etc.
    
    * UPDATE: Progress is current to the ITEM DECISION-TO-ACCESS Module, new
    comments to suggest control flows or consider circumstances of
    user-to-system process, new methods for ITEM DTA Module, and need to
    creat fourth InsVar for GROUP DTA Module, etc.
    
    p.s. Consider using inheritance for next features.
    
    * UPDATE: Enchanced UI(Not Final), current work and progress is in the
    Item Module
    
    * UPDATE: Almost Same With May 16
    
    * UPDATE: PROPER Flow for Password Verification
    
    * UPDATE: Managing the Accounts, and Creating another METHODS and
    PREDEFINED PROCESSES -> NEXT:SEO for Accs. for Retrieval and Editing or
    deleting
    
    * UPDATE: Majorly in Managing accounts, Next is Creating new Account
    method and having a SEO for the accounts that might duplicate and for
    editing the accounts.
    
    * Major Cleaning of Flow: Current Progress is in Items and Groups
    
    * UPDATED FLOW
    
    * UPDATE ALMOST AT 75% haha
    
    * UPDATED: MORE PROGRESS 80%+ in reaching the ideal system
    
    * 90% REMOVE ITEM SAKA ID NUMBER NG ITEMS NA LANG, Recheck if may
    nakaligtaan
    
    * ADJUSTMENTS MAY 23
    
    ---------
    
    Co-authored-by: Jerry Aaron <Jerry Aaron@Alphie>

--- finals_InventoryTrackingSys/src/finals_InventoryTrackingSys/Main.java
+++ finals_InventoryTrackingSys/src/finals_InventoryTrackingSys/Main.java
@@ -31,16 +31,23 @@ public class Main {
 	                    	break;
 	                    	
 	                    case GROUP, CATEGORY:
-	                    	System.out.println("\t | ACCESS A GROUP OR CATEGORY");
+	                    	Logics_Inventory.chooseGroupOption();
+	                    	Logics_Inventory.getFourthDecision();
 	                		break;
 	            		
+	                    case MANAGEACCOUNT:
+	                    	Logics_Inventory.manageAccount();
+	                    	Logics_Inventory.firstDecisionDuplicate();
+	                    	break;
+	                    	
 	                    case EXIT:
-	                    	Logics_Inventory.profileUser();
-	                    	Logics_Inventory.firstDecision();
+	                    	Logics_Inventory.profileUserDuplicate();
+	                    	Logics_Inventory.firstDecisionDuplicate();
 	                    	break;
 	            			
 	                    case DEFAULT:
 	                    	System.out.println("\t | EXITING SYSTEM");
+	                    	Logics_Inventory.firstDecision();
 	            			break;
 	            			
 	                    default:
